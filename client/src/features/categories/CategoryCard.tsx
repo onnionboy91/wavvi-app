128398bfbb77ee6e207d5d343a5c8f48de23330f
Viktor <onnionboy@gmail.com>
2024-02-10 12:21:40 +0300

    deleteBtn

--- client/src/features/categories/CategoryCard.tsx
+++ client/src/features/categories/CategoryCard.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import { Category } from './types'
 import { Link } from 'react-router-dom'
+import { removeCategory } from './categoriesSlice'
+import { useAppDispatch } from '../../redux/store'
 
 const CategoryCard = ({category}: {category: Category}): JSX.Element => {
+const dispatch = useAppDispatch()
   return (
     <>
     <div className="card" style={{margin: '50px'}} >
@@ -10,8 +13,8 @@ const CategoryCard = ({category}: {category: Category}): JSX.Element => {
   <div className="card-body">
     <h5 className="card-title">{category.name}</h5>
     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" >Подробнее</a>
     <Link className="btn btn-primary" to={`/categories/${category.id}`}>More information</Link>
+    <button className="btn btn-danger" onClick={() => dispatch(removeCategory(category.id))} type='button'>Удалить</button>
   </div>
 </div>
 </>
