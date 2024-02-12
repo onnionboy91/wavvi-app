848149a67e8f430ceeb22221c4581b040fb0139c
Gonamen <sashakim123321@gmail.com>
2024-02-12 11:26:54 +0300

    point-2

--- client/src/features/videos/CategoryVideosItem.tsx
+++ client/src/features/videos/CategoryVideosItem.tsx
@@ -25,7 +25,7 @@ const CategoryVideosItem = ({video}:VideosItemProps) => {
     <img src={video.img} className="card-img-top" alt="..."/>
     <button onClick={openModal}>Open Modal</button>
       <Modal isOpen={modalOpen} onClose={closeModal}>
-        <iframe src={video.content}></iframe>
+        <iframe width="560" height="315" src={video.content} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
       </Modal>
   <div className="card-body">
     <h5 className="card-title">{video.name}</h5>
@@ -34,8 +34,10 @@ const CategoryVideosItem = ({video}:VideosItemProps) => {
 
   </div>
 </div>
+{/* <div data-poster-url="https://assets-global.website-files.com/5dbb40d6d8c97447e9450447/6195bc3f9e9c991b6505ab49_GRID_STYLES_LANDING_PAGE_1-poster-00001.jpg" data-video-urls="https://assets-global.website-files.com/5dbb40d6d8c97447e9450447/6195bc3f9e9c991b6505ab49_GRID_STYLES_LANDING_PAGE_1-transcode.mp4,https://assets-global.website-files.com/5dbb40d6d8c97447e9450447/6195bc3f9e9c991b6505ab49_GRID_STYLES_LANDING_PAGE_1-transcode.webm" data-autoplay="true" data-loop="true" data-wf-ignore="true" data-beta-bgvideo-upgrade="false" className="background-video updated w-background-video w-background-video-atom"><video id="98f8b142-c4aa-3696-4aef-7a3dcef0c0b3-video" autoplay="" style="background-image:url(&quot;https://assets-global.website-files.com/5dbb40d6d8c97447e9450447/6195bc3f9e9c991b6505ab49_GRID_STYLES_LANDING_PAGE_1-poster-00001.jpg&quot;)" playsinline="" data-wf-ignore="true" data-object-fit="cover"><source src="https://assets-global.website-files.com/5dbb40d6d8c97447e9450447/6195bc3f9e9c991b6505ab49_GRID_STYLES_LANDING_PAGE_1-transcode.mp4" data-wf-ignore="true" /><source src="https://assets-global.website-files.com/5dbb40d6d8c97447e9450447/6195bc3f9e9c991b6505ab49_GRID_STYLES_LANDING_PAGE_1-transcode.webm" data-wf-ignore="true" /></video> 
+</div> */}
 </>
-  )
+)
 }
 
 export default CategoryVideosItem
