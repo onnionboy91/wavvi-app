ff431acc060f79b9dbc0adc9c21eb25e6bb8f9a6
JuliaLap <jul.lapshina2013@gmail.com>
2024-02-12 19:30:34 +0300

    FavAndInstructorPage

--- client/src/features/videos/CategoryVideos.tsx
+++ client/src/features/videos/CategoryVideos.tsx
@@ -1,31 +1,29 @@
-import React, { useEffect } from 'react'
-import { useParams } from "react-router-dom"
-import { loadVideos } from "./videosSlice"
-import { useAppDispatch } from "../../redux/store"
-import { useSelector } from "react-redux"
-import { RootState } from '../../redux/store'
-import CategoryVideosItem from "./CategoryVideosItem"
+import React, { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { loadVideos } from './videosSlice';
+import { useAppDispatch } from '../../redux/store';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../redux/store';
+import CategoryVideosItem from './CategoryVideosItem';
 
 const CategoryVideos = () => {
-  const dispatch = useAppDispatch()
-  const { categoryId } = useParams()
-  const videos = useSelector((store: RootState) => store.videos.videos)
+  const dispatch = useAppDispatch();
+  const { categoryId } = useParams();
+  const videos = useSelector((store: RootState) => store.videos.videos);
 
-  console.log(videos)
-
+  console.log(videos);
 
   useEffect(() => {
-    dispatch(loadVideos(+categoryId!)).catch(console.log)
-  },[])
+    dispatch(loadVideos(+categoryId!)).catch(console.log);
+  }, []);
 
   return (
     <div>
       {videos.map((video) => (
-        <CategoryVideosItem key={video.id} video={video}/>
-    ))}
+        <CategoryVideosItem key={video.id} video={video} />
+      ))}
     </div>
+  );
+};
 
-  )
-}
-
-export default CategoryVideos
+export default CategoryVideos;
